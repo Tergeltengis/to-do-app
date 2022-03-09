e1c7b91cb85d74e6f36c8f6c90249b5463bd7d5a
Gansukh Batjargal <gansukhb@Gansukhs-MacBook-Air.local>
2022-03-09 21:47:43 +0800

    confgured CI

--- tests/testUtils/setupEnvironment.ts
+++ tests/testUtils/setupEnvironment.ts
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 import Task from "../../models/tasks";
 
 const setupEnvironment = async () => {
-  process.env.MONGODB_HOST = "mongodb://localhost:27017/todo_test";
+  if (!process.env.MONGODB_HOST) {
+    process.env.MONGODB_HOST = "mongodb://localhost:27017/todo_test";
+  }
   if (mongoose.connection.readyState === 0) {
     await mongoose.connect(process.env.MONGODB_HOST, {});
     await Task.deleteMany();
